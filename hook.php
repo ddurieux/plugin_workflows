56b87744f75e932215179d5714ee67b15389114a
jdudon <jessica.dudon@gmail.com>
2019-07-15 09:38:00 +0200

    git init

---
+++ hook.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ -------------------------------------------------------------------------
+ workflows plugin for GLPI
+ Copyright (C) 2019 by the workflows Development Team.
+
+ https://github.com/pluginsGLPI/workflows
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of workflows.
+
+ workflows is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ workflows is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with workflows. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
+/**
+ * Plugin install process
+ *
+ * @return boolean
+ */
+function plugin_workflows_install() {
+   return true;
+}
+
+/**
+ * Plugin uninstall process
+ *
+ * @return boolean
+ */
+function plugin_workflows_uninstall() {
+   return true;
+}
